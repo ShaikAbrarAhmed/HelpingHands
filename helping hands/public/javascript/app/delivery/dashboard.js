3be24d2b946cb84727cc6a5e9e0dc6a6469b8b71
ShaikAbrarAhmed <146211312+ShaikAbrarAhmed@users.noreply.github.com>
2023-11-24 19:13:05 +0530

    Add files via upload

---
+++ helping hands/public/javascript/app/delivery/dashboard.js
@@ -0,0 +1,112 @@
+$(window).on('load', initialize);
+
+const classes = {
+    type:{
+        cancelled:"table-danger",
+        approved: "table-success",
+        waiting:  "table-warning"
+    }
+}
+
+let types = {
+    approved : {
+        label:"Approved",
+        isSelected:false
+    },
+    waiting : {
+        label:"Wait",
+        isSelected:false
+    },
+    cancelled : {
+        label:"Cancel",
+        isSelected:false
+    }
+};
+
+function getSelect(selected){
+  let html =  Object.keys(types).map(type => {
+        return ` <option ${type == selected ? 'selected="true"' : ""} value="${type}">${types[type].label}</option>`
+  }).join('');
+  return  `<select class="custom-select status" name="status">${html}</select>`
+}
+
+function initialize() {
+    let orderDetailsTable = new Table({
+        target:'#order-details',
+        columns:[
+            {title:'S.No'},
+            {title:"category"},
+            {title:"item"},
+            {title:"option"},
+            {title:"AadharNumber"},
+          //  {title:"status"},
+            
+        ],
+        formatter: formatTableResponse
+    })
+
+    function formatTableResponse(data, from=0){
+        return data.map(function(row,index){
+            return {
+                attributes: {
+                    id: row._id
+                },
+                classes:classes.type[row.status],
+                data: {
+                    Sno:`${(from + (index + 1))}`,
+                    category: row.category,
+                    requirement1: row.requirement1,
+                    requirement2: row.requirement2,
+                    adhaarNumber: row.adhaarNumber,
+                   // status : getSelect(row.status || 'ok'),
+                    
+                }
+            }
+        })
+    }
+    
+    orderDetailsTable.render(`/api/app`);
+    $('#order-details').on('change','select.status',function(){
+        let value = $(this).val();
+        let _classes = Object.keys(classes.type).map(e => classes.type[e]).join(' ');
+        let _class = classes.type[value] || "";
+
+        let $tr = $(this).parents('tr').first();
+        let id = $tr.data('id');
+       
+        $tr.removeClass(_classes).addClass(_class);
+
+        let data = {
+            status: value
+        }
+
+        $.ajax({
+            url: `/api/app/${id}`,
+            method: 'put',
+            cache: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function () {
+                Swal.fire(
+                    'Success!',
+                    'Order status updated successfully.',
+                    'success'
+                )
+            },
+            error: function () {
+                Swal.fire(
+                    'Error!',
+                    'Something went wrong.',
+                    'error'
+                );
+            }
+        })
+    })
+}
+
+
+
+
+
+
